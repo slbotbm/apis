c32f956febc043d789d75ca2ebad985400f33664
slbotbm <rimukshkansal@gmail.com>
2023-10-30 10:43:15 +0900

    changes

--- first-api/app/Http/Controllers/Api/V1/CustomerController.php
+++ first-api/app/Http/Controllers/Api/V1/CustomerController.php
@@ -6,10 +6,10 @@ use App\Models\Customer;
 use Illuminate\Http\Request;
 use App\Filters\V1\CustomersFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
+use App\Http\Requests\V1\StoreCustomerRequest;
 
 class CustomerController extends Controller
 {
@@ -72,7 +72,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
